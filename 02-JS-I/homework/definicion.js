6333ab195d50a9c9446518b9594e507257c950a9
herinyhert <herinyhert@gmail.com>
2022-02-02 10:59:55 -0300

    completado

---
+++ 02-JS-I/homework/definicion.js
@@ -0,0 +1,34 @@
+//variable:
+//Se puede definir una variable como un objeto que esta guardado dentro de una 
+//caja, el cual es identificada con un nombre, esto nos va a permitir utilizar 
+//el contenio de esa caja en cualquier momento que nombremos la caja.
+
+//Strins:
+//Es un conjunto de texto encerrado entre comillas simples ('')ó dobles (""),
+//el mismo se refiere a un tipo de contenido de una variable.
+
+//Funciones:
+//Se refieren a soluciones de problemas o pedidos, su funcion es simplificar
+//el proceso de escritura en nuestro archivo
+
+//Argumento:
+//Definimos como argumentos cuando queremos utilizar una variable dentro de una
+//funcion, donde puede ser identificada antes o despues de la funcion.
+
+//Return:
+//Se utiliza return cuando solicitamos a la funcion que nos devuelva un valor,
+//es importe tener en cuenta que una vez que le indicamos a la función return la misma
+//se detiene y no continua la ejecución
+
+//Declaraciones if:
+//la declarración if es lo que conocemos comunmente como "si se cumple esta 
+//condición", como por ejemplo "si esta lloviendo" se debe usar una sombrilla,
+//estas declaraciones las vamos a usar dentro de las funciones y su enfoque principal
+//es ejercutar diferentes tipos de respuestas segun sus condiciones de pregunta.
+
+//Valores Booleanos:
+//True, refiere al verdadero o mejor conocido como verdad
+//false, refiere a falso
+
+//Estos valores son utilizados en las funciones como verificadores si cumplen con
+//las condiciones solicitadas.
