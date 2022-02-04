5acfbfa6ca4504c38a423e487a52442567e8595f
herinyhert <herinyhert@gmail.com>
2022-02-03 21:08:17 -0300

    completado

---
+++ 03-JS-II/homework/conceptos.js
@@ -0,0 +1,15 @@
+// `for`
+// podemos definirla como un Expresion que repite unas "X" cantidad de veces hasta llegar al resultado que deseamos,
+// esta compuesta por tres datos (inicio de la variable, condicion de variable, incremento de la variable)
+
+// `&&`
+//es un operador que refiere al "AND", este evalua dos expresiones en conjunto, onde du valor es verdadero
+//si alguna de las dos expresiones es falsa su resultado final siempre sera falso.
+
+// `||`
+// es conocido como el "OR" permite evaluar dos expresiones por separado, si alguna de las dos es verdadera
+// su resultado es true, en caso de que las dos sean falsa su resultado sera false, 
+
+// `!`
+// es conocido como "NOT", es mismo refiere a una negacion de la expresion informada, podemos dar un ejemplo como
+//  1 !> 2 su resultado es "True"
