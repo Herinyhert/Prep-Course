3c692de2f15c0d62c7338aef2be119376eaf3863
herinyhert <herinyhert@gmail.com>
2022-02-04 19:58:49 -0300

    completado

---
+++ 04-JS-III/homework/conceptos.js
@@ -0,0 +1,4 @@
+// Array
+// Definimos como array cuando queremos trabajar con mas de una exprecion en una variable.
+// anteriormente comentamos las diferentes expresiones que tiene una variable y siempre refiriendonos
+// como unica expresion, en el caso de los Array podemos trabajar con mas de una expresion a la vez.
