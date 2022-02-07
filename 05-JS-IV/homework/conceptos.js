c1b9cb7edd6a1695feef3aa53d01c728843e3e83
herinyhert <herinyhert@gmail.com>
2022-02-07 09:50:23 -0300

    completado

---
+++ 05-JS-IV/homework/conceptos.js
@@ -0,0 +1,34 @@
+//Objetos:
+// podemos visualizar el objeto como el contenido de un array, a diferencia que mientras el array se puede tomar como una lista y el objeto es
+// el contenido de un dato en especifico,como ejemplo podemos mostrar:
+//array: 
+//Alumnos = [Ana, Juan, Mariana] 
+// objetos: 
+//ana=[nombre:"", apellido:"", edad:"", carrera:"", promedio:""]
+//Juan=[nombre:"", apellido:"", edad:"", carrera:"", promedio:""]
+//Mariana=[nombre:"", apellido:"", edad:"", carrera:"", promedio:""]
+
+//Propiedades:
+// podemos definirla como la relacion entre un nombre y un valor, el valor lo podemos conocer como metodos.
+
+
+//Métodos:
+// Los metodos son lo que hoy conocemos como una función, es una acción que queremos que se ejecute al llamar nuestra variable.
+
+
+//Bucle `for…in`:
+//Bucles: lo definimos como un una verificacion que se repite tantas veces como lo solicitemos, a esto se le llama interación.
+//for: lo usamos cuando se trabaje con indices numericos. -> for(var i=0; i<9; i++)
+
+// for      (var i=0;                             i<9;                                           i++)
+//    indicamo donde comienza a contar          Definimos donde para de contar                  comienza el conteno de nuevo
+
+// in: se utiliza cuando no tenemos indices numericos y la busquedad se realiza a travez de la palabra clave y el nombre del objeto
+// de esta forma el bucle realiza la busquedad bajo los parametros informados. -> for(let clave in usuario)
+// for(    let clave                     in usuario)
+//      definimos la variable            definimos el nombre del objeto
+
+//Notación de puntos vs notación de corchetes:
+//Notacion de Puntos, lautilizamos cuando queremos llamar el nombre del objeto, como por ejemplo una propiedad.
+// notacion de corchetes, la utilizamos cuando queremos llamar una matriz (cadena, numero, variable), en caso de una clave tendremos que usar comillas
+
