6110fc6bb06605c0dd64a212b4f1c8a4823d24fd
herinyhert <herinyhert@gmail.com>
2022-02-09 21:33:51 -0300

    completado

---
+++ 06-JS-V/homework/conceptos.js
@@ -0,0 +1,6 @@
+//`prototype`
+//Podemos definirlo com una plantilla, donde le daremos el acceso al objeto cada vez que lo necesitemos, sin la necesidad de volver a escribir
+// too el metodo de nuevo, permitiendo que no llenemos la memoria de la computadora con sobre escritura innecesaria.
+
+// _Constructors_ (de Clases)
+// son funciones diseñada para crear objetos desde una base, esa funcion va a tener las propiedades que de los objetos proximos a utilizar. 
