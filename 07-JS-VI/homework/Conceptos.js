92b4ead5807b250eafe1d5012b8e3cbd1f9b4b93
herinyhert <herinyhert@gmail.com>
2022-02-10 18:09:44 -0300

    Completado

---
+++ 07-JS-VI/homework/Conceptos.js
@@ -0,0 +1,3 @@
+//Funciones Callback
+//Una función de callback es una función que se pasa a otra función como un argumento, que luego sera invoca dentro 
+//de la función externa para completar una acción
